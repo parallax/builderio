b29dacbdaed8e7e31229a72142cba4ca0634aec0
Liam Declan Kilkenny <l.kilkenny@MacBook-Pro-5.local>
2023-06-09 19:15:29 +0100

    feat: initial foundation for builderio app

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+
+  /**
+   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
+   * out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
+
+  experimental: {
+    scrollRestoration: true,
+    appDir: true,
+  },
+
+  // i18n: {
+  //   locales: ["en"],
+  //   defaultLocale: "en",
+  // },
+};
+export default config;
