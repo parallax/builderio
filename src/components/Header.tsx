5301132e9ae4c1a47b52fe7d8ce01f1aadabdebd
Sam Sutherland <sam.sutherland@parall.ax>
2023-06-10 18:27:46 +0100

    fix: style logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header(props: { title: string; logo: string }) {
   return (
     <div className="bg-white flex w-full justify-center shadow-lg">
       <div className="flex w-full max-w-7xl items-center px-8">
-        <div className="relative mr-4 h-[50px] w-[50px] my-2">
+        <div className="relative mr-3 h-[40px] w-[40px] my-2 rounded-lg overflow-hidden">
           <Image alt="header-image" fill src={props?.logo} />
         </div>
         <div className="flex-1 font-bold font-lg">{props?.title}</div>
